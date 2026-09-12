ea8fae7cff7bd9413f67ba41ecc13d88224cd9e3
labonishow <126092468+labonishow@users.noreply.github.com>
2026-09-12 15:48:06 +0530

    expense tracker app port add

--- backend/config/database.js
+++ backend/config/database.js
@@ -6,7 +6,13 @@ const sequelize = new Sequelize(
   process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 3306,
     dialect: "mysql",
+    dialectOptions: {
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    },
     logging: false,
   }
 );
